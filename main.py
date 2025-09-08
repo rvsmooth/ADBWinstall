7f5a27eeb7b127d56798606719658907f7530619
rvsmooth <riveeks.smooth@gmail.com>
2025-09-08 16:43:53 +0530

    feat: wait for userinput to quit, improve main()
    * main()
    - set env variable if it is a clean install only
    - update platform-tools only if it is update

--- main.py
+++ main.py
@@ -7,23 +7,41 @@ url = "https://dl.google.com/android/repository/platform-tools-latest-windows.zi
 tools_path = os.path.abspath("C:\\platform-tools")
 extract_path = os.path.abspath("C:\\")
 file_name = "platform-tools.zip"
+adb_location = os.path.join(tools_path, "adb.exe")
+is_adb_installed = shutil.which("adb")
+is_fb_installed = shutil.which("fastboot")
 
 
-def main():
-    print("Downloading latest platform-tools")
-    urllib.request.urlretrieve(url, file_name)
+def main(install=None, update=None):
+    if install or update:
+        print("Downloading latest platform-tools")
+        urllib.request.urlretrieve(url, file_name)
 
-    print(f"Extracting platform-tools to {extract_path}")
-    shutil.unpack_archive(file_name, extract_path, "zip")
+        print(f"Extracting platform-tools to {extract_path}")
+        shutil.unpack_archive(file_name, extract_path, "zip")
 
-    print("Adding platform-tools to path")
-    if shutil.which("adb"):
-        print("adb is already added to the path\nskipping adding it to env")
-    else:
+    if install:
         command_arg = f"{os.environ['PATH']};{tools_path}"
         subprocess.run(["setx", "/M", "PATH", command_arg])
         print("Setup complete")
 
 
-# main execution
-main()
+if is_adb_installed and is_fb_installed:
+    print(
+        f"Platform-tools are already installed at \n{is_adb_installed}\n{is_fb_installed}"
+    )
+    if is_adb_installed == adb_location:
+        print(f"platform-tools are installed at {adb_location}")
+        answer = input("Do you want to update them to the latest version(y/n)")
+        if answer.lower() == "y":
+            main(update=True)
+        else:
+            print("You have choosen not to update them.\nAborting...")
+elif not is_adb_installed:
+    print("platform-tools are not installed.")
+    print(f"Installing  platform-tools to {adb_location}")
+    main(install=True)
+
+
+# wait for userinput to exit
+input("Press any key to exit")
