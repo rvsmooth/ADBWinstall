67d84421b38ce7cdb008e3e7b279ec2cf5a0ae7f
rvsmooth <riveeks.smooth@gmail.com>
2025-09-08 17:06:56 +0530

    feat: avoid duplication of env variable and update path

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ file_name = "platform-tools.zip"
 adb_location = os.path.join(tools_path, "adb.exe")
 is_adb_installed = shutil.which("adb")
 is_fb_installed = shutil.which("fastboot")
+installed_tools_path = os.path.dirname(f"{is_adb_installed}")
+
+envs = os.environ["PATH"].split(";")
 
 
 def main(install=None, update=None):
@@ -21,16 +24,17 @@ def main(install=None, update=None):
         shutil.unpack_archive(file_name, extract_path, "zip")
 
     if install:
-        command_arg = f"{os.environ['PATH']};{tools_path}"
-        subprocess.run(["setx", "/M", "PATH", command_arg])
-        print("Setup complete")
+        if tools_path not in envs:
+            command_arg = f"{os.environ['PATH']};{tools_path}"
+            subprocess.run(["setx", "/M", "PATH", command_arg])
+            print("Setup complete")
 
 
 if is_adb_installed and is_fb_installed:
     print(
         f"Platform-tools are already installed at \n{is_adb_installed}\n{is_fb_installed}"
     )
-    if is_adb_installed == adb_location:
+    if installed_tools_path == tools_path:
         print(f"platform-tools are installed at {adb_location}")
         answer = input("Do you want to update them to the latest version(y/n)")
         if answer.lower() == "y":
