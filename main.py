9b20d217279bd751259665230faf3f4e8c1f0e2b
rvsmooth <riveeks.smooth@gmail.com>
2025-09-08 17:26:14 +0530

    refactor

--- main.py
+++ main.py
@@ -6,48 +6,60 @@ import urllib.request
 url = "https://dl.google.com/android/repository/platform-tools-latest-windows.zip"
 tools_path = os.path.abspath("C:\\platform-tools")
 extract_path = os.path.abspath("C:\\")
+
 file_name = "platform-tools.zip"
-adb_location = os.path.join(tools_path, "adb.exe")
 is_adb_installed = shutil.which("adb")
 is_fb_installed = shutil.which("fastboot")
 installed_tools_path = os.path.dirname(f"{is_adb_installed}")
 
+# get all envs
 envs = os.environ["PATH"].split(";")
 
 
+def prep_tools():
+    print("Downloading latest platform-tools")
+    urllib.request.urlretrieve(url, file_name)
+
+    print(f"Extracting platform-tools to {extract_path}")
+    shutil.unpack_archive(file_name, extract_path, "zip")
+
+
+def add_path():
+    command_arg = f"{os.environ['PATH']};{tools_path}"
+    subprocess.run(["setx", "/M", "PATH", command_arg])
+    print("Setup complete")
+
+
+# main function
 def main(install=None, update=None):
     if install or update:
-        print("Downloading latest platform-tools")
-        urllib.request.urlretrieve(url, file_name)
-
-        print(f"Extracting platform-tools to {extract_path}")
-        shutil.unpack_archive(file_name, extract_path, "zip")
+        prep_tools()
 
     if install:
         if tools_path not in envs:
-            command_arg = f"{os.environ['PATH']};{tools_path}"
-            subprocess.run(["setx", "/M", "PATH", command_arg])
-            print("Setup complete")
+            print("Adding platform-tools to PATH")
+            add_path()
         else:
             print("platform-tools are already in PATH")
 
 
+# execution
 if is_adb_installed and is_fb_installed:
-    print(
-        f"Platform-tools are already installed at \n{is_adb_installed}\n{is_fb_installed}"
-    )
+    print(f"Platform-tools are already installed at\n{installed_tools_path}")
+
     if installed_tools_path == tools_path:
-        print(f"platform-tools are installed at {adb_location}")
+        print(f"platform-tools are installed at {tools_path}")
         answer = input("Do you want to update them to the latest version(y/n)")
+
         if answer.lower() == "y":
             main(update=True)
         else:
             print("You have choosen not to update them.\nAborting...")
+
 elif not is_adb_installed:
     print("platform-tools are not installed.")
-    print(f"Installing  platform-tools to {adb_location}")
+    print(f"Installing  platform-tools to {tools_path}")
     main(install=True)
-
 
 # wait for userinput to exit
 input("Press any key to exit")
